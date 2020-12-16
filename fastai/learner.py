ad6c05a1172a19f022bbbcc7cad4e4b3be5041b6
Ivan Pereira <navi1921@gmail.com>
2020-12-16 17:09:53 -0300

    Update learner.py

--- fastai/learner.py
+++ fastai/learner.py
@@ -158,28 +158,20 @@ class Learner():
     def all_batches(self):
         print('abcp4')
         self.n_iter = len(self.dl)
-        #for i in range(self.n_iter):
-           #if(i<10000):
-           #   raise SkipItemException
-           #else:
-           #o=next(iter(self.dl))
-           #if(i%100==0):
-           #print(i)
-             #print(o)
         for o in enumerate(self.dl): self.one_batch(*o)
 
     def _do_one_batch(self):
-        #self.pred = self.model(*self.xb)
-        #self('after_pred')
-        #if len(self.yb): self.loss = self.loss_func(self.pred, *self.yb)
-        #self('after_loss')
-        #if not self.training or not len(self.yb): return
-        #self('before_backward')
-        #self._backward()
-        #self('after_backward')
+        self.pred = self.model(*self.xb)
+        self('after_pred')
+        if len(self.yb): self.loss = self.loss_func(self.pred, *self.yb)
+        self('after_loss')
+        if not self.training or not len(self.yb): return
+        self('before_backward')
+        self._backward()
+        self('after_backward')
         self._step()
-        #self('after_step')
-        #self.opt.zero_grad()
+        self('after_step')
+        self.opt.zero_grad()
 
     def one_batch(self, i, b):
         self.iter = i
