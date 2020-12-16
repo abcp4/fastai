f0cbb8d2d1c134c633cd110da8cf684bfbf05ff6
Ivan Pereira <navi1921@gmail.com>
2020-12-15 23:36:26 -0300

    Update learner.py

--- fastai/learner.py
+++ fastai/learner.py
@@ -163,15 +163,15 @@ class Learner():
     def _do_one_batch(self):
         self.pred = self.model(*self.xb)
         self('after_pred')
-        if len(self.yb): self.loss = self.loss_func(self.pred, *self.yb)
+        #if len(self.yb): self.loss = self.loss_func(self.pred, *self.yb)
         self('after_loss')
-        if not self.training or not len(self.yb): return
+        #if not self.training or not len(self.yb): return
         self('before_backward')
         #self._backward()
         self('after_backward')
         #self._step()
         self('after_step')
-        self.opt.zero_grad()
+        #self.opt.zero_grad()
 
     def one_batch(self, i, b):
         self.iter = i
